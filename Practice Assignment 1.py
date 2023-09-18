fffe82d700aedceab2b41ca15035fbc0ae14763b
Ahmed Antar <32421421+AhmedAntar@users.noreply.github.com>
2023-09-18 11:22:33 +0300

    Update Practice Assignment 1.py

--- Practice Assignment 1.py
+++ Practice Assignment 1.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-get_ipython().run_line_magic('matplotlib', 'inline')
+%matplotlib inline
 
 import pandas as pd
 import numpy as np
 
 import folium 
+import seaborn as sns
 
+%matplotlib inline
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# In[17]:
 
 
 URL = "C:/Users/ahmed.antar/Downloads/Historical_Wildfires.csv"
@@ -26,45 +18,24 @@ df = pd.read_csv(path)
 
 print(df)
 
-
-# In[18]:
-
-
 #top five rows of data set
 df.head()
-
-
-# In[19]:
-
 
 #column names
 df.columns
 
-
-# In[20]:
-
-
 #data type
 df.dtypes
-
-
-# In[10]:
-
 
 import datetime as dt
 
 df['Year'] = pd.to_datetime(df['Date']).dt.year
 df['Month'] = pd.to_datetime(df['Date']).dt.month
 
-
-# In[11]:
-
-
 #df.columns
 df.dtypes
 
-
-# In[26]:
+#######Task 1.1:
 
 
 df_line = df.groupby(df['Year'])['Automobile_Sales'].mean()
@@ -78,26 +49,169 @@ plt.text(1982, 650, '1981-82 Recession')
 plt.legend()
 plt.show()
 
+#######Task 1.2:
 
-# In[ ]:
+df_Mline = df.groupby(['Year','Vehicle_Type'], as_index=False)['Automobile_Sales'].sum()
+df_Mline.set_index('Year', inplace=True)
+df_Mline = df_Mline.groupby(['Vehicle_Type'])['Automobile_Sales']
+df_Mline.plot(kind='line')
+plt.xlabel('Year')
+plt.ylabel('Sales')
+plt.title('Sales Trend Vehicle-wise during Recession')
+plt.legend()
+plt.show()
+
+###TASK 1.3: Use the functionality of Seaborn Library to create a visualization to compare the sales trend per vehicle type for a recession period with a non-recession period.
+
+new_df = df.groupby('Recession')['Automobile_Sales'].mean().reset_index()
+
+# Create the bar chart using seaborn
+
+# sns.barplot(x='Recession', y='Automobile_Sales', hue='Recession',  data=new_df)
+# plt.xlabel('Sales Period')
+# plt.ylabel('Sales Volum')
+# plt.title('Average Automobile Sales during Recession and Non-Recession')
+# plt.xticks(ticks=[0, 1], labels=['Non-Recession', 'Recession'])
+# plt.show()
 
 
 
+# Filter the data for recessionary periods
+recession_data = df[df['Recession'] == 1]
+dd=df.groupby(['Recession','Vehicle_Type'])['Automobile_Sales'].mean().reset_index()
+
+# Create the grouped bar chart using seaborn
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Recession', y='Automobile_Sales', hue='Vehicle_Type', data=dd)
+plt.xticks(ticks=[0, 1], labels=['Non-Recession', 'Recession'])
+plt.xlabel('Period')
+plt.ylabel('Average Sales')
+plt.title('Vehicle-Wise Sales during Recession and Non-Recession Period')
+
+plt.show()
+
+#######Task 1.4:
+
+rec_data = df[df['Recession'] == 1]
+non_rec_data = df[df['Recession'] == 0]
+
+#Figure
+fig=plt.figure(figsize=(12, 6))
+
+#Create different axes for subploting
+ax0 = fig.add_subplot(1, 2, 1) # add subplot 1 (1 row, 2 columns, first plot)
+ax1 = fig.add_subplot(1 , 2, 2 ) # add subplot 2 (1 row, 2 columns, second plot). 
+
+#plt.subplot(1, 2, 1)
+sns.lineplot(x='Year', y='GDP', data=rec_data, label='Recession', ax=ax0)
+ax0.set_xlabel('Year')
+ax0.set_ylabel('GDP')
+ax0.set_title('GDP Variation during Recession Period')
+
+#plt.subplot(1, 2, 2)
+sns.lineplot(x='Year', y='GDP', data=non_rec_data, label='non-Recession',ax=ax1)
+ax1.set_xlabel('Year')
+ax1.set_ylabel('GDP')
+ax1.set_title('GDP Variation during non-Recession Period')
+
+plt.tight_layout()
+plt.show()
+
+##TASK 1.5: Develop a Bubble plot for displaying the impact of seasonality on Automobile Sales.
+
+non_rec_data = df[df['Recession'] == 0]
+    
+size=non_rec_data['Seasonality_Weight'] #for bubble effect
+
+sns.scatterplot(data=non_rec_data, x='Month', y='Automobile_Sales', size=size)
+
+#you can further include hue='Seasonality_Weight', legend=False)
+
+plt.xlabel('Month')
+plt.ylabel('Automobile_Sales')
+plt.title('Seasonality impact on Automobile Sales')
+
+plt.show()
+
+#TASK 1.6: Use the functionality of Matplotlib to develop a scatter plot to identify the correlation...
+#...between average vehicle price relate to the sales volume during recessions. 
+
+rec_data = df[df['Recession'] == 1]
+plt.scatter(recession_data['Price'], rec_data['Automobile_Sales'])
+
+plt.xlabel('Price')
+plt.ylabel('Automobile_Sales')
+plt.title('Average Vehicle Price and Sales during Recessions')
+plt.show()
+
+#TASK 1.7: Create a pie chart to display the portion of advertising expenditure of XYZAutomotives during recession...
+#...and non-recession periods.
 
 
-# In[ ]:
+# Filter the data 
+Rdata = df[df['Recession'] == 1]
+NRdata = df[df['Recession'] == 0]
+
+# Calculate the total advertising expenditure for both periods
+RAtotal = Rdata['Advertising_Expenditure'].sum()
+NRAtotal = NRdata['Advertising_Expenditure'].sum()
+
+# Create a pie chart for the advertising expenditure 
+plt.figure(figsize=(8, 6))
+
+labels = ['Recession', 'Non-Recession']
+sizes = [RAtotal, NRdata['Advertising_Expenditure'].sum()]
+#print(sizes)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+
+plt.title('Advertising Expenditure during Recession and Non-Recession Periods')
+
+plt.show()
+
+#### TASK 1.8: Develop a pie chart to display the total Advertisement expenditure for each vehicle type during recession period.
+
+# Filter the data 
+Rdata = df[df['Recession'] == 1]
+
+# Calculate the sales volume by vehicle type during recessions
+VTsales = Rdata.groupby('Vehicle_Type')['Advertising_Expenditure'].sum()
+
+# Create a pie chart for the share of each vehicle type in total sales during recessions
+plt.figure(figsize=(8, 6))
+
+labels = VTsales.index
+sizes = VTsales.values
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+
+plt.title('Share of Each Vehicle Type in Total Sales during Recessions')
+
+plt.show()
+
+
+#TASK 1.9:Develop a countplot to analyse the effect of the unemployment rate on vehicle type and sales during the Recession Period.
+
+data= df[df['Recession'] == 1]
+    
+plt.figure(figsize=(10, 6))
+
+sns.countplot(data=data, x='unemployment_rate', hue='Vehicle_Type')
+
+plt.xlabel('Unemployment Rate')
+plt.ylabel('Count')
+plt.title('Effect of Unemployment Rate on Vehicle Type and Sales')
+plt.legend(loc='upper right')
+plt.show()
 
 
 
+path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/us-states.json'
 
+df3 = pd.read_json(path)
+print(df3)
+filename = "us-states.json"
 
-# In[ ]:
+#Task: 2.1
 
-
-
-
-
-# In[ ]:
 
 
 
